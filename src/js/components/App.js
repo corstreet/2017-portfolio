ea65d80816298d79a5d25967d5d31243e7ac7f19
Corwin <corstreet@gmail.com>
2017-09-01 18:27:17 -0700

    adjustments to routing

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Home from '../routes/Home';
@@ -8,18 +8,20 @@ import Work from '../routes/Work';
 
 
 class App extends React.Component {
+
 	render() {
 		return (
-			<BrowserRouter>
+			<HashRouter hashType="noslash">
 				<div>
 				  <Navbar />
 			      <Route exact path="/" component={Home}/>
 			      <Route path="/about" component={About}/>
 			      <Route path="/work" component={Work}/>
 			    </div>
-			</BrowserRouter>
+			</HashRouter>
 		)
 	}
+
 }
 
 export default App;

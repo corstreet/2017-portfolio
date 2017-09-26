99ea7dd21f78a1a015911546a7016d2f019618e6
Corwin <corstreet@gmail.com>
2017-09-26 14:14:58 -0700

    some layout updates --WIP

--- src/js/routes/Work.js
+++ src/js/routes/Work.js
@@ -4,8 +4,20 @@ class Work extends React.Component {
 
 	render() {
 		return (
-			<div className="container">
-				<h1>WORKY WORK</h1>
+			<div className="container work-container">
+				<div className="row">
+					<img className="project-image" src="./assets/images/ch-desktop.png" alt=""/>
+					<div className="project-details">
+						<div className="project-tools">
+							<h1>Collecalth</h1>
+							<p>Words for you. Some more for me the doctor. Hamburgers, with corn sauce aoili and broccoli bread.</p>
+						</div>
+						<div className="project-blurb">
+							<h1>Collective Health</h1>
+							<p>Words for you. Some more for me the doctor. Hamburgers, with corn sauce aoili and broccoli bread.</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}

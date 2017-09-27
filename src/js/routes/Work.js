b5fe328b5a647065234992218b8579245cf1d6db
corwins <corwin_street@apple.com>
2017-09-26 23:37:13 -0700

    updates tracked files, new project page design

--- src/js/routes/Work.js
+++ src/js/routes/Work.js
@@ -6,15 +6,22 @@ class Work extends React.Component {
 		return (
 			<div className="container work-container">
 				<div className="row">
-					<img className="project-image" src="./assets/images/ch-desktop.png" alt=""/>
+					<figure className="project-image">
+						<img src="./assets/images/ch-grab.jpg" alt=""/>
+					</figure>
 					<div className="project-details">
-						<div className="project-tools">
-							<h1>Collecalth</h1>
-							<p>Words for you. Some more for me the doctor. Hamburgers, with corn sauce aoili and broccoli bread.</p>
-						</div>
-						<div className="project-blurb">
-							<h1>Collective Health</h1>
-							<p>Words for you. Some more for me the doctor. Hamburgers, with corn sauce aoili and broccoli bread.</p>
+						<h1 className="h1">Collective Health</h1>
+						<p>Collective Health gives companies a smarter way to provide healthcare coverage through technology. During my time there, I was able to contribute to many front end initiatives.</p>
+						<ul className="tech-list">
+							<li className="tech-item">Backbone.js</li>
+							<li className="tech-item">PHP</li>
+							<li className="tech-item">jQuery</li>
+							<li className="tech-item">Sass/SCSS</li>
+							<li className="tech-item">Custom WP</li>
+							<li className="tech-item">Grunt</li>
+						</ul>
+						<div className="project-link">
+							<a href="javascript:;">View Project</a>
 						</div>
 					</div>
 				</div>
